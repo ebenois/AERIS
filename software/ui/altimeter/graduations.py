da9c3f279ab5bdf4f361fcbeb173402012a4e69a
ebenois <ebenois@ensc.fr>
2026-01-23 17:34:48 +0100

    Correction de bug de l'horizon artificiel et optimization de l'altimètre

--- software/ui/altimeter/graduations.py
+++ software/ui/altimeter/graduations.py
@@ -2,45 +2,48 @@ from PyQt6.QtWidgets import QGraphicsItemGroup, QGraphicsLineItem, QGraphicsText
 from PyQt6.QtGui import QColor, QPen, QFont
 from PyQt6.QtCore import Qt
 
-class AltitudeGraduations(QGraphicsItemGroup):
+
+class AltitudeGraduations(QGraphicsItemGroup):     
     def __init__(self, parent=None, width=600):
         super().__init__(parent)
+
+        self.width = width
         self.graduationsData = []
-        
+
         pen = QPen(QColor("#FFFFFF"), 4)
         pen.setCapStyle(Qt.PenCapStyle.RoundCap)
         font = QFont("Arial", 17)
 
-        for i in range(-100*2, 100*2):
-            altitudeValue = i * 50
-            
-            line = QGraphicsLineItem(-width/2, 0, -width/2+10, 0, self)
+        for i in range(-10, 10 + 1):
+            line = QGraphicsLineItem(-width / 2, 0, -width / 2 + 12, 0, self)
             line.setPen(pen)
 
             text = None
             if i % 2 == 0:
-                text = QGraphicsTextItem(str(int(altitudeValue)), self)
+                text = QGraphicsTextItem("", self)
                 text.setDefaultTextColor(Qt.GlobalColor.white)
                 text.setFont(font)
 
             self.graduationsData.append({
-                'line': line,
-                'text': text,
-                'width': width,
-                'altitudeLevel': altitudeValue
+                "index": i,
+                "line": line,
+                "text": text
             })
 
     def updatePositions(self, altitude):
+        baseAltitude = int(altitude // 100) * 100
+
         for grad in self.graduationsData:
-            altitudeValue = grad['altitudeLevel']
+            offset = grad["index"] * 50
+            altitudeValue = baseAltitude + offset
 
-            yLocal = (altitude - altitudeValue)
+            y_local = altitude - altitudeValue
 
-            grad['line'].setPos(0, yLocal)
+            grad["line"].setPos(0, y_local)
 
-            if grad['text']:
-                t = grad['text']
-                width = grad['width']
-                
-                t.setPos(-width/3, yLocal - t.boundingRect().height() / 2)
-
+            if grad["text"]:
+                grad["text"].setPlainText(str(int(altitudeValue)))
+                grad["text"].setPos(
+                    -self.width / 3,
+                    y_local - grad["text"].boundingRect().height() / 2
+                )
